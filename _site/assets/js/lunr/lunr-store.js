126e94179bbaaf5e3c7745d6f237004648cd23be
samjjc <sam111jc@gmail.com>
2019-03-20 10:32:22 -0400

    git cache cleared

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,6 +0,0 @@
-var store = [{
-        "title": "Rules of Icing (🍞 Edition)",
-        "excerpt":"Icing History In ancient Greek history, King Sisyphus was made to roll a huge boulder endlessly up a steep hill. The maddening nature of the punishment was reserved for King Sisyphus due to his hubristic belief that his cleverness surpassed that of Zeus himself. Zeus accordingly displayed his own cleverness...","categories": ["icing"],
-        "tags": [],
-        "url": "http://localhost:4000/icing/2019/03/18/icing-rules.html",
-        "teaser":null}]
